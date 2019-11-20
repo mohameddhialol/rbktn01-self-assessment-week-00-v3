0655ebea0ade8ebb4323f98ff2037305213effb9
AdamMomen <adammuman81@gmail.com>
2019-11-20 09:31:17 +0100

    solved

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -50,7 +50,23 @@
 
 
 
-var filterFamilyMembers = function (familyTree, truthTest) {
-  // All your code in this function body
-};
+var filterFamilyMembers = function (familyTree, truthTest, result, index) {
+	// All your code in this function body
+	
+	var result = result || []; //the container for the results
+	var index = index || 0; //index onf the child in the children array
 
+	if (truthTest(familyTree)) { 
+		result.push((familyTree).firstName + ' ' + familyTree.lastName) //if he\she passes the test the full nale is pushed into the result array
+	}
+
+	if (familyTree.children.length !== 0) {
+		filterFamilyMembers(familyTree.children[index],truthTest,result,0) // if he\she has children we should go deeper into the family tree but with no return bc it will break the function
+	}
+
+	if (familyTree.children.length > index) {
+		return filterFamilyMembers(familyTree.children[index],truthTest,result,index++) // if there are still children in the array we should apply the test to them to there's a return here bc w must return the final result 
+	}
+
+	return result;
+};
